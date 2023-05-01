8e8277ea0248dacbde0f70c59581f78b6904e708
Ansh Gupta <anshgupta072003@gmail.com>
2023-05-01 13:15:52 +0530

    unused imports

--- model.py
+++ model.py
@@ -1,18 +1,14 @@
 # %%
-import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score   
 
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import train_test_split
 
 
 
 # %%
 # Read original dataset
-from random import seed
 
 
 iris_df = pd.read_csv('./dataset/Iris.csv')
